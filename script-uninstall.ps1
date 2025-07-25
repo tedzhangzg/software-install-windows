391491d0e8048d7ef5151617496397d3a37e9f57
Ted Zhang <83065920+tedzhangzg@users.noreply.github.com>
2025-07-25 13:23:13 +0800

    Create script-uninstall.ps1

---
+++ script-uninstall.ps1
@@ -0,0 +1,32 @@
+# script-uninstall.ps1
+# ==================================================
+# Description
+# ==================================================
+# Usage
+# ==================================================
+
+
+Write-Host "Starting script-uninstall.ps1 ..."
+
+# include
+. ".\functions.ps1"
+. ".\urls.ps1"
+. ".\values.ps1"
+
+# var
+# $var = ""
+
+
+# uninstall Java
+
+
+
+Write-Host ""
+
+Write-Host "Terminating script-uninstall.ps1 ..."
+pause
+
+
+# ==================================================
+# Notes
+# ==================================================
