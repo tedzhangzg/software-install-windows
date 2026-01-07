13b1156f50c00e550e59623dc68d8a825cdbf48b
TedZhang <tedzhangzg@gmail.com>
2026-01-07 13:22:03 +0800

    updated script to disable update

--- script-disablewinupdate.ps1
+++ script-disablewinupdate.ps1
@@ -1,1 +1,35 @@
+# script-disablewinupdate.ps1
+# ==================================================
+# Description
+# ==================================================
+# Usage
+# ==================================================
 
+
+Write-Host "Starting script-disablewinupdate.ps1 ..."
+
+# include
+# . ".\functions.ps1"
+# . ".\urls.ps1"
+# . ".\values.ps1"
+
+# var
+# $var = ""
+
+Write-Host ""
+Write-Host "================================================================================"
+Write-Host "Script - Disable Windows Update"
+Write-Host "================================================================================"
+Write-Host ""
+
+
+
+Write-Host ""
+
+Write-Host "Terminating script-disablewinupdate.ps1 ..."
+pause
+
+
+# ==================================================
+# Notes
+# ==================================================
